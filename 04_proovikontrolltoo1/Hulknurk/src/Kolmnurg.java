f2e2259f75a1d42ea24755d6b692ac24b89892d1
Jelizaveta Kruglova <114922000+anjufehno@users.noreply.github.com>
2023-04-21 13:48:04 +0300

    Update Kolmnurg.java

--- 04_proovikontrolltoo1/Hulknurk/src/Kolmnurg.java
+++ 04_proovikontrolltoo1/Hulknurk/src/Kolmnurg.java
@@ -3,9 +3,15 @@ import java.util.List;
 public class Kolmnurg {
     List<Double> xCoords;
     List<Double> yCoords;
-
-    public Kolmnurg(List<Double> xCoords, List<Double> yCoords){
+    public Kolmnurg(List<Double> xCoords, List<Double> yCoords) {
         this.xCoords = xCoords;
         this.yCoords = yCoords;
     }
+
+    public double lisaJaAnnaYmbermoot(double newX, double newY){
+        xCoords.add(newX);
+        yCoords.add(newY);
+
+    }
+
 }
