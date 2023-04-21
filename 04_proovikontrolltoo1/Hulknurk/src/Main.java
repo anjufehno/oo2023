791457f991bef61ea28ad294311b20b0961683f4
Jelizaveta Kruglova <114922000+anjufehno@users.noreply.github.com>
2023-04-21 13:50:35 +0300

    Update Main.java

--- 04_proovikontrolltoo1/Hulknurk/src/Main.java
+++ 04_proovikontrolltoo1/Hulknurk/src/Main.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Double> xCoords = new ArrayList<>(Arrays.asList(1.5, 2.5, 3.5));
-        List<Double> yCoords = new ArrayList<>(Arrays.asList(4.5, 5.5, 6.5));
 
+        List<Double> xCoords = new ArrayList<>(Arrays.asList(1.5, 2.5, 3.5));
+        List<Double> yCoords = new ArrayList<>(Arrays.asList(3.5, 4.5, 5.5));
         Kolmnurg kolmnurg = new Kolmnurg(xCoords, yCoords);
+        System.out.println(kolmnurg.xCoords);
+        System.out.println(kolmnurg.yCoords);
     }
 }
