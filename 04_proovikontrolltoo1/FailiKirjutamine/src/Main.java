c1e865a95ebf130cf4c27b19a3c1ffbdaf862127
Jelizaveta Kruglova <114922000+anjufehno@users.noreply.github.com>
2023-04-21 13:54:54 +0300

    Update Main.java

--- 04_proovikontrolltoo1/FailiKirjutamine/src/Main.java
+++ 04_proovikontrolltoo1/FailiKirjutamine/src/Main.java
@@ -1,14 +1,22 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("lugemine.txt"));
         String rida = br.readLine();
-        while (rida != null){
+        while (rida != null) {
             System.out.println(rida);
             String[] tykeldus = rida.split("u");
             System.out.println(Arrays.toString(tykeldus));
+            System.out.println(tykeldus.length);
+            double ruut = Math.pow(tykeldus.length, 2);
+            System.out.println(ruut);
+            double ruutjuur = Math.sqrt(tykeldus.length);
+            System.out.println(ruutjuur);
             rida = br.readLine();
         }
     }
