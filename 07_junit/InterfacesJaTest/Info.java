48df4e6425d5ed353478a8db998f7bc7c090d606
Jelizaveta Kruglova <114922000+anjufehno@users.noreply.github.com>
2023-04-28 12:40:27 +0300

    Delete 07_junit/InterfacesJaTest directory

--- 07_junit/InterfacesJaTest/Info.java
+++
@@ -1,7 +0,0 @@
-package InterfacesJaTest;
-
-public interface Info {
-    /* Интерфейс не обладает телом метода, тоесть интерфейс не содержит ту часть когда, которая
-    пишется между { фигурных скобок } */
-    public void showInfo();
-}
